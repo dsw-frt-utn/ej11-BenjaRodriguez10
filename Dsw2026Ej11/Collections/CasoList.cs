7c5627ddeabaed1ad019c69fd7b9248bc57d5469
Maximo Benjamin Rodriguez <maximobenjaminrodriguez01@gmail.com>
2026-05-21 20:01:52 -0300

    TP11 completado: Implementa ejemplo de List, Dictionary y LINQ

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,9 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +13,39 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> alumnos;
+    public CasoList()
+    {
+        alumnos = new List<Alumno>();
+    }
+    public void AgregarAlumno(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+    public List<Alumno> ObtenerListaAlumnos()
+    {
+        return alumnos;
+    }
+
+    public Alumno BuscarAlumnoPorNombre(string nombre)
+    {
+        return alumnos.FirstOrDefault(a => a.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    public void EliminarAlumno(Alumno alumno)
+    {
+        alumnos.Remove(alumno);
+    }
+
+    public void EliminarAlumnoEnPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < alumnos.Count)
+        {
+            alumnos.RemoveAt(posicion);
+        }
+        else
+        {
+            Console.WriteLine("La posición ingresada no es válida.");
+        }
+    }
 }
